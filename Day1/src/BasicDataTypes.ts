78375c16a80f65892fc87c72414a7cec00040517
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-22 00:09:12 +0600

    Basic data type: number, string, boolean, array, null, undefined, tuple

---
+++ Day1/src/BasicDataTypes.ts
@@ -0,0 +1,26 @@
+// Basic data types
+
+// String
+let firstName : string = "Ashraful"; // explicit data type
+// Number
+let roll : number = 10;
+// boolean
+let isAdmin : boolean = true;
+// Undefined
+let x: undefined = undefined;
+// Null
+let y : null = null;
+
+let d : number;
+d = 12;
+
+// Array
+
+let sotan : string[] = ['Saminur', 'Farhan'];
+let sotanAge : number[] = [25, 23];
+sotanAge.push(45);
+
+// Tuple --> array --> order --> Types of values
+let coordinates : [number,number] = [1, 5]
+let ageName : [number, string, boolean] = [21, "Ashraf", true]
+
