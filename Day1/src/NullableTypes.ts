60cfaf00d34e4f4689a50f45bec2c95ba0fe764c
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-22 04:46:23 +0600

    Never,Unknown And Nullable Type

---
+++ Day1/src/NullableTypes.ts
@@ -0,0 +1,34 @@
+{
+    // Nullable Types
+    const searchName = (value: string | null) => {
+        if (value) {
+            console.log('Searching');
+        } else {
+            console.log('Nothing to search');
+        }
+    }
+    searchName(null);
+
+    // Unknown types
+    const getSpeedInMeterPerSecond = (value: unknown) => {
+        if (typeof value === 'number') {
+            const convertedSpeed = (value * 1000) / 3600
+            console.log(`The speed is ${convertedSpeed} ms^-1`);
+        }
+        else if (typeof value === 'string') {
+            const [v, unit] = value.split(' ');
+            const convertedSpeed = (parseFloat(v) * 1000) / 3600
+            console.log(`The speed is ${convertedSpeed} ms^-1`);
+        }
+        else {
+            console.log('Wrong Input');
+        }
+    }
+    getSpeedInMeterPerSecond(null);
+
+    // Never type
+    const throwError = (msg: string): never => {
+        throw new Error(msg);  
+    }
+    throwError('Uff error peye gelam')
+}
