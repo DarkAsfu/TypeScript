b347b8661460d267530b9ac89a6d45b7c7487813
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-22 00:16:04 +0600

    update

---
+++ Day1/dist/BasicDataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// Basic data types
+// String
+let firstName = "Ashraful"; // explicit data type
+// Number
+let roll = 10;
+// boolean
+let isAdmin = true;
+// Undefined
+let x = undefined;
+// Null
+let y = null;
+let d;
+d = 12;
+// Array
+let sotan = ['Saminur', 'Farhan'];
+let sotanAge = [25, 23];
+sotanAge.push(45);
+console.log(sotanAge);
+// Tuple --> array --> order --> Types of values
+let coordinates = [1, 5];
+let ageName = [15, "Ashraf", true];
