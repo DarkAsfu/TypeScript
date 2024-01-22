3c1633188518f54d5539ba3795168ebb89154fa2
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-22 22:25:47 +0600

    Type Assertion

---
+++ Day2/TypeAssertionNarrowing.ts
@@ -0,0 +1,29 @@
+{
+    // type assertion
+    let anything: any;
+    anything = "Next Level Web Development";
+    anything = 222;
+    // (anything as number).
+
+    const kgToGm = (value: string | number) => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The converted value is: ${convertedValue}`;
+        } else if (typeof value === 'number') {
+            return `The converted value is: ${value * 1000}`;
+        }
+    }
+    const result2 = kgToGm(2) as number
+    const result1 = kgToGm('2') as string
+
+    type CustomError = {
+        message: string
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log((error as CustomError).message);
+    }
+    // console.log();
+}
